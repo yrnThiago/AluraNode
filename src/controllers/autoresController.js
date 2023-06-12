fc1577ea3165a2b8d8abde2c04f0f3431c183869
yrnThiago <thiagodias_123@hotmail.com>
2023-06-12 11:26:42 -0300

    feat: add validations

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -8,7 +9,7 @@ class AutorController {
     
       res.status(200).json(autoresResultados);
     } catch (err) {
-      res.status(500).json({message: "Erro interno no servidor!"});
+      res.status(500).json({message: "Erro interno no servidor."});
     }
   };
 
@@ -17,9 +18,17 @@ class AutorController {
       const {id} = req.params;
       const autorResultado = await autores.findById(id);
 
-      res.status(200).send(autorResultado);
+      if(autorResultado !== null) {
+        res.status(200).json(autorResultado);
+      } else {
+        res.status(404).json({message: "Id do autor não localizado."});
+      }
     } catch (err) {
-      res.status(400).send({message: `${err.message} - Id do autor não localizado.`});
+      if(err instanceof mongoose.Error.CastError) {
+        res.status(400).json({message: "Um ou mais dados fornecidos estão incorretos."});
+      } else {
+        res.status(500).json({message: "Erro interno do servidor."});
+      }
     }
   };
 
@@ -39,7 +48,7 @@ class AutorController {
       const {id} = req.params;
       await autores.findByIdAndUpdate(id, {$set: req.body});
 
-      res.status(200).send({message: "autor atualizado com sucesso!"});
+      res.status(200).send({message: "autor atualizado com sucesso."});
     } catch (err) {
       res.status(500).send({message: `${err.message} - falha ao atualizar autor.`});
     }
@@ -50,7 +59,7 @@ class AutorController {
       const {id} = req.params;
       await autores.findByIdAndDelete(id);
 
-      res.status(200).send({message: "autor excluído com sucesso!"});
+      res.status(200).send({message: "autor excluído com sucesso."});
     } catch (err) {
       res.status(500).send({message: `${err.message}`});
     }
