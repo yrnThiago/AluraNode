b70cb77d262c6be887393fce6173249050f7ed28
yrnThiago <thiagodias_123@hotmail.com>
2023-06-13 07:35:34 -0300

    fix: refactor Erros

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(err, req, res, next) {
-  console.log(`ERROR: ${err.name}\nNAME_ERROR: ${err.message}\nSTACK_ERROR: ${err.stack}`);
-
   if(err instanceof mongoose.Error.CastError) {
-    res.status(400).json({message: "Um ou mais dados fornecidos estão incorretos."});
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if(err instanceof mongoose.Error.ValidationError) {
-    const mensagensErro = Object.values(err.errors)
-      .map(err => err.message)
-      .join("; ");
-    
-
-    res.status(400).json({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    new ErroValidacao(err).enviarResposta(res);
   } else {
-    res.status(500).json({message: "Erro interno do servidor."});
+    new ErroBase().enviarResposta(res);
   }
 }
 
